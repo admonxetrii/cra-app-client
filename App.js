354ed594174050c607fc7248b8b3e6df11439f5c
Nisham Wagle <nishamwagle@gmail.com>
2021-12-01 07:39:54 +0545

    eslint added

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import React from 'react';
-import { StatusBar, SafeAreaView, Platform, StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-const isAndroid = Platform.OS === 'android';
 export default function App() {
   return (
     <>
-    <SafeAreaView style={{flex:1, marginTop: StatusBar.currentHeight }}>
-      <View style={{padding: 16, backgroundColor: 'green'}}>
-        <Text >Search</Text>
-      </View>
-      <View style={{flex: 1, padding: 16, backgroundColor: 'pink', justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Ma project work garirako babe </Text>
-      </View>
-    </SafeAreaView>
-    <ExpoStatusBar style ="auto" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.lists}>
+          <Text>This is the body area </Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+
+  lists: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
